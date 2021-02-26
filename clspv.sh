b55977697536c641d6d8c8c84492fe9bda361676
Reese <reeselevine@ucsc.edu>
2021-02-25 21:51:42 -0800

    Start to generify code

--- clspv.sh
+++ clspv.sh
@@ -1,1 +1,1 @@
-/shared/clspv-expir/build/bin/clspv --cl-std=CL2.0 --inline-entry-points $1.cl -o $1.spv
+/shared/clspv/build/bin/clspv --cl-std=CL2.0 --inline-entry-points $1.cl -o $1.spv
--- lamports-bakery.cpp
+++ lamports-bakery.cpp
@@ -3,42 +3,76 @@
 #include <vector>
 #include <set>
 #include <string>
+#include <chrono>
+# include <iostream>
 
 const int minWorkgroups = 36;
 const int maxWorkgroups = 36;
 const int minWorkgroupSize = 24;
 const int maxWorkgroupSize = 24;
+const int iterationsPerTest = 1000;
 const int numIterations = 10;
 
 using Array = vuh::Array<uint32_t,vuh::mem::Host>;
+using SpecConstants = vuh::typelist<uint32_t>;
+
 class FenceProfiler {
 
 public:
-    void run() {
+    void run(char* testName) {
+	std::cout << "Running test " << testName << "\n";
         // setup devices, memory, and parameters
         auto instance = vuh::Instance();
         auto device = instance.devices().at(0);
+	std::vector<Array> buffers;
+	if (testName == "lamports-bakery") {
+	    initializeLamportsBakery(&device, buffers);
+	}
         auto entering = Array(device, maxWorkgroups);
 	auto tickets = Array(device, maxWorkgroups);
 	auto var = Array(device, 1);
         auto paramsBuffer = Array(device, 2);
-        using SpecConstants = vuh::typelist<uint32_t>;
 	std::string testFile("lamports-bakery.spv");
-	for (int i = 0; i < 10; i++) {
-	    printf("\ntest iteration %i\n", i);
+	std::chrono::duration<double> results[numIterations];
+	double sum = 0;
+	for (int i = 0; i < numIterations; i++) {
+	    std::chrono::time_point<std::chrono::system_clock> start, end;
+	    std::cout << "\n test iteration " << i << "\n";
 	    int numWorkgroups = setNumWorkgroups();
 	    int workgroupSize = setWorkgroupSize();
             clearMemory(entering, maxWorkgroups);
 	    clearMemory(tickets, maxWorkgroups);
 	    clearMemory(var, 1);
 	    paramsBuffer[0] = numWorkgroups;
-	    paramsBuffer[1] = numIterations;
+	    paramsBuffer[1] = iterationsPerTest;
             auto program = vuh::Program<SpecConstants>(device, testFile.c_str());
+	    program.grid(numWorkgroups);
+	    program.spec(workgroupSize);
+	    start = std::chrono::system_clock::now();
             program.grid(numWorkgroups).spec(workgroupSize)(entering, tickets, var, paramsBuffer);
-	    int expectedCount = numIterations * numWorkgroups;
-	    printf("expected: %i, actual: %u\n", expectedCount, var[0]);
+	    end = std::chrono::system_clock::now();
+	    int expectedCount = iterationsPerTest * numWorkgroups;
+	    std::chrono::duration<double> result = end - start;
+	    sum += result.count();
+	    std::cout << "iteration time: " << result.count() << "s\n";
+	    std::cout << "expected: " << expectedCount << ", actual: " << var[0] << "\n";
 	}
+	std::cout << "Average test iteration time: " << sum / numIterations << "s\n";
     }
+
+    void initializeLamportsBakery(vuh::Device* device, std::vector<Array> &buffers) {
+   	buffers.push_back(Array(*device, maxWorkgroups));
+        buffers.push_back(Array(*device, maxWorkgroups));
+        buffers.push_back(Array(*device, 1));
+    }
+
+    void bindLamportsBakery(vuh::Program<SpecConstants> *program, std::vector<Array> &buffers, Array *paramsBuffer) {
+	clearMemory(buffers[0], maxWorkgroups);
+	clearMemory(buffers[1], maxWorkgroups);
+	clearMemory(buffers[2], 1);
+    	program->bind(buffers[0], buffers[1], buffers[2], *paramsBuffer); 
+    }
+
 
     void clearMemory(Array &gpuMem, int size) {
 	for (int i = 0; i < size; i++) {
@@ -66,10 +100,14 @@ public:
 };
 
 int main(int argc, char* argv[]) {
+    if (argc != 2) {
+	std::cout << "Test name must be specified\n";
+	return 1;
+    }
     srand (time(NULL));
     FenceProfiler app;
     try {
-        app.run();
+        app.run(argv[1]);
     }
     catch (const std::runtime_error& e) {
         printf("%s\n", e.what());
